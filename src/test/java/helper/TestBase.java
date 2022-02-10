5b7dd0d98d2276c5ecef89fe11fc3aca4ff09e64
abhinavminhas <abhinavminhas@users.noreply.github.com>
2022-02-10 16:40:53 +1100

    TestBase - Refactoring

--- src/test/java/helper/TestBase.java
+++ src/test/java/helper/TestBase.java
@@ -6,7 +6,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -15,7 +14,6 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 @Test
