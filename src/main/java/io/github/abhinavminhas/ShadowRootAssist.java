fc279316e7e2e7af7608b6a7d0eccf715c3c3c00
abhinavminhas <abhinavminhas@users.noreply.github.com>
2022-02-10 16:09:31 +1100

    isShadowRootElementPresent() - Addtion

--- src/main/java/io/github/abhinavminhas/ShadowRootAssist.java
+++ src/main/java/io/github/abhinavminhas/ShadowRootAssist.java
@@ -1,7 +1,6 @@
 package io.github.abhinavminhas;
 
 import java.util.Map;
-
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
@@ -17,7 +16,7 @@ import org.openqa.selenium.remote.RemoteWebElement;
  * @author 	Abhinav Minhas
  * @version 1.0.0
  * @since 	01-01-2022
- * */
+ */
 public class ShadowRootAssist {
 	
 	/** 
@@ -29,7 +28,7 @@ public class ShadowRootAssist {
 	 * @param pollingIntervalInMilliseconds	Polling interval time in milliseconds.
 	 * @return								Shadow root web element.
 	 * @exception WebDriverException		Throws - 'WebDriverException' in case any shadow root element is not found.
-	 * */
+	 */
 	public static WebElement getShadowRootElement(WebDriver webDriver, String shadowRootSelector, int timeInSeconds, int pollingIntervalInMilliseconds) {
 		WebElement requiredShadowRoot = null;
 		String shadowRootQuerySelector = "return document.querySelector('%s').shadowRoot";
@@ -47,11 +46,54 @@ public class ShadowRootAssist {
 				requiredShadowRoot = remoteWebElement;
 			} else
 				requiredShadowRoot = (WebElement)returnedObject;
-		}
-		catch(WebDriverException ex) {
+		} catch(WebDriverException ex) {
 			throw new WebDriverException(String.format("%s: Shadow root element for selector '%s' Not Found.", new Object(){}.getClass().getEnclosingMethod().getName(), shadowRootSelector));
 		}
 		return requiredShadowRoot;
+	}
+	
+	/**
+	 * Checks if the shadow root element exists or not.
+	 * 
+	 * @param webDriver						Selenium web driver instance.
+	 * @param shadowRootSelector			Shadow root element selectors (probably jQuery or CssSelectors).
+	 * @param throwError					Boolean value to throw error if shadow root element hierarchy does not exists.
+	 * @param timeInSeconds					Wait time in seconds.
+	 * @param pollingIntervalInMilliseconds Polling interval time in milliseconds.
+	 * @return								Boolean value shadow root element exists or not.
+	 * @exception WebDriverException		Throws - 'WebDriverException' in case any shadow root element is not found when @param 'throwError' is set to 'true'.
+	 */
+	public static Boolean isShadowRootElementPresent(WebDriver webDriver, String shadowRootSelector, Boolean throwError, int timeInSeconds, int pollingIntervalInMilliseconds) {
+		Boolean isPresent = false;
+		String shadowRootQuerySelector = "return document.querySelector('%s').shadowRoot";
+		String shadowRootElement = String.format(shadowRootQuerySelector, shadowRootSelector);
+        try {
+        	WebDriverWait webDriverWait = new WebDriverWait(webDriver, timeInSeconds, pollingIntervalInMilliseconds);
+			webDriverWait.until((ExpectedCondition<Boolean>) wd -> ((JavascriptExecutor)webDriver).executeScript(shadowRootElement) != null); 
+			Object returnedObject = ((JavascriptExecutor)webDriver).executeScript(shadowRootElement);
+			if (returnedObject instanceof Map) {
+				@SuppressWarnings("unchecked")
+				Map<String, Object> map = (Map<String, Object>)returnedObject;
+				webDriverWait.until(item -> map != null);
+				RemoteWebElement remoteWebElement = new RemoteWebElement();
+				remoteWebElement.setParent((RemoteWebDriver)webDriver);
+				remoteWebElement.setId((String)map.values().iterator().next());
+				Object requiredShadowRoot = remoteWebElement;
+				webDriverWait.until(item -> requiredShadowRoot instanceof WebElement);
+				isPresent = true;
+				return isPresent;
+			} else {
+				webDriverWait.until(item -> returnedObject instanceof WebElement);
+				isPresent = true;
+				return isPresent;
+			}
+        } catch (WebDriverException ex) {
+        	if (throwError)
+                throw new WebDriverException(String.format("%s: Shadow root element for selector '%s' Not Found.", new Object(){}.getClass().getEnclosingMethod().getName(), shadowRootSelector));
+            else
+                isPresent = false;
+        }
+        return isPresent;
 	}
 	
 }
--- src/test/java/shadowrootdigger/tests/ShadowRootDiggerChromeSettingsTests.java
+++ src/test/java/shadowrootdigger/tests/ShadowRootDiggerChromeSettingsTests.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import helper.TestBase;
 import io.github.abhinavminhas.ShadowRootAssist;
 
@@ -14,14 +13,14 @@ public class ShadowRootDiggerChromeSettingsTests extends TestBase {
     private String _notExistsShadowRootElement = "not-exists";
     
 	@Test(description = CHROME_SETTINGS_TESTS)
-	public void test_GetShadowRootElement_ChromeSettings_ShadowRootElementExists() {
+	public void test_getShadowRootElement_ChromeSettings_ShadowRootElementExists() {
         WebDriver.navigate().to("chrome://settings/clearBrowserData");
         WebElement clearBrowsingTab = ShadowRootAssist.getShadowRootElement(WebDriver, _existsShadowRootElement, 20 , 2000);
         Assert.assertNotNull(clearBrowsingTab);
     }
 	
 	@Test(description = CHROME_SETTINGS_TESTS)
-	public void test_GetShadowRootElement_ChromeSettings_ShadowRootElementDoesNotExists() {
+	public void test_getShadowRootElement_ChromeSettings_ShadowRootElementDoesNotExists() {
 		String expectedErrorMessage = "getShadowRootElement: Shadow root element for selector 'not-exists' Not Found.";
         WebDriver.navigate().to("chrome://settings/clearBrowserData");
         try {
@@ -31,4 +30,26 @@ public class ShadowRootDiggerChromeSettingsTests extends TestBase {
         catch (AssertionError ex) { throw ex; }
         catch (WebDriverException ex) { Assert.assertTrue(ex.getMessage().contains(expectedErrorMessage)); }
     }
+	
+	@Test(description = CHROME_SETTINGS_TESTS)
+	public void test_isShadowRootElementPresent_ChromeSettings_ShadowRootExists() {
+		WebDriver.navigate().to("chrome://settings/clearBrowserData");
+		Boolean exists = ShadowRootAssist.isShadowRootElementPresent(WebDriver, _existsShadowRootElement, false, 20, 2000);
+		Assert.assertEquals(true, exists);
+	}
+	
+	@Test(description = CHROME_SETTINGS_TESTS)
+	public void test_isShadowRootElementPresent_ChromeSettings_ShadowRootNotExists() {
+		String expectedErrorMessage = "isShadowRootElementPresent: Shadow root element for selector 'not-exists' Not Found.";
+		WebDriver.navigate().to("chrome://settings/clearBrowserData");
+        Boolean exists = ShadowRootAssist.isShadowRootElementPresent(WebDriver, _notExistsShadowRootElement, false, 20, 2000);
+        Assert.assertEquals(false, exists);
+        try {
+        	ShadowRootAssist.isShadowRootElementPresent(WebDriver, _notExistsShadowRootElement, true, 20, 2000);
+            Assert.fail("No Exception Thrown.");
+        }
+        catch (AssertionError ex)  { throw ex; }
+        catch (WebDriverException ex) { System.out.println(ex.getMessage()); Assert.assertTrue(ex.getMessage().contains(expectedErrorMessage)); }
+	}
+	
 }
