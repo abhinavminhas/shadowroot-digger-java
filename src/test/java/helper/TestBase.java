6576a0417e7a02391097fd1ed91ec4f0845cc159
abhinavminhas <abhinavminhas@users.noreply.github.com>
2022-03-12 12:03:23 +1100

    Bump Chromedriver version '98.0.4758.102' -> '99.0.4844.51'

--- src/test/java/helper/TestBase.java
+++ src/test/java/helper/TestBase.java
@@ -21,7 +21,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class TestBase {
 
 	protected WebDriver webDriver;
-	private String chromeDriverVersion = "98.0.4758.102";
+	private String chromeDriverVersion = "99.0.4844.51";
 	protected static final String CHROME_SETTINGS_TESTS = "CHROME-SETTINGS-TESTS";
 	protected static final String SHADOW_DOM_HTML_TESTS = "SHADOW-DOM-HTML-TESTS";
 	protected static Properties config;
