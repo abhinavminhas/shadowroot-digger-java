d08c8d007dca4239f8e0bb11c057b714f92c1afd
abhinavminhas <abhinavminhas@users.noreply.github.com>
2023-06-24 10:32:00 +1000

    Bump Selenium version '4.9.1' -> '4.10.0'

--- src/main/java/io/github/abhinavminhas/ShadowRootAssist.java
+++ src/main/java/io/github/abhinavminhas/ShadowRootAssist.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
  * 'ShadowRootAssist' class to support digging of shadow roots in DOM.
  * 
  * @author 	Abhinav Minhas
- * @version 2.1.20
+ * @version 2.1.21
  * @since 	01-01-2022
  */
 public class ShadowRootAssist {
